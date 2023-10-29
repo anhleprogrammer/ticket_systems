b45eb5c9904e784787f7567d827ce52ba6766896
Anh Le <anh4dle@gmail.com>
2023-10-29 18:04:00 -0400

    edit type config for react-icons

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,4 +1,4 @@
-declare module "*.svg" {
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-  export default content;
+declare module "react-icons" {
+  const noTypesYet: any>;
+  export default noTypesYet;
 }
