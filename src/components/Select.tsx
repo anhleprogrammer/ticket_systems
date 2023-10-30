79a1092ed72d66c36fa84c9513c2020166ac695f
Anh Le <anh4dle@gmail.com>
2023-10-30 18:32:09 -0400

    fixed an important part

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/Io";
-function Select({ options }) {
+function Select() {
   const opts = ["Low", "Medium", "High"];
+  const [state, setState] = useState(opts);
   return (
     <div className="flex items-center gap-4 hover:cursor-default">
       Select
       <IoIosArrowDown />
-      {/* <IoIosArrowUp /> */}
+      <IoIosArrowUp />
     </div>
   );
 }
